ab05540c860f6174eda49d0cffc566c17d74e9ef
Gabriel Barukel <gabrukel@gmail.com>
2024-05-03 20:15:12 -0300

    finaly ready to merge(refactoring in main)

--- src/Components/CarouselItem.jsx
+++ src/Components/CarouselItem.jsx
@@ -4,7 +4,7 @@ const CarouselItem = ({ movie, index }) => {
   return (
     <div className="item-container" style={{ translate: `${-100 * index}%` }}>
       <img
-        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
         className="carousel-img"
       />
     </div>
--- src/Components/MovieItem.jsx
+++ src/Components/MovieItem.jsx
@@ -1,43 +1,39 @@
 import { useState } from "react";
 
 const MovieItem = ({ movieData }) => {
-    const [showExtraInfo, setShowExtraInfo] = useState(false);
-  
-    const extraInfoHandler = () => {
-      if (!showExtraInfo) {
-        setShowExtraInfo(true);
-      } else {
-        setShowExtraInfo(false);
-      }
-    };
-  
-    const imgSRC = `https://image.tmdb.org/t/p/w500${movieData.poster_path}`;
-  
-    return (
-      <li className='item'>
-        <div
-          key={movieData.id}
-          className={showExtraInfo ? 'itemContainer' : ''}
-        >
-          <img
-            src={imgSRC}
-            alt="Poster original de la película"
-            className={!showExtraInfo ? 'poster' : 'extraInfoPoster'}
-            onClick={extraInfoHandler}
-          />
-          {showExtraInfo && (
-            <div
-              className={
-                showExtraInfo ? 'extraInfoShown' : 'extraInfoHide'
-              }
-            >
-              <h3 className='extraInfoTitle'>{movieData.title}</h3>
-              <p className='extraInfoText'>{movieData.overview}</p>
-            </div>
-          )}
-        </div>
-      </li>
-    );
+  const [showExtraInfo, setShowExtraInfo] = useState(false);
+
+  const extraInfoHandler = () => {
+    if (!showExtraInfo) {
+      setShowExtraInfo(true);
+    } else {
+      setShowExtraInfo(false);
+    }
   };
-  
-  export default MovieItem;
+
+  const imgSRC = `https://image.tmdb.org/t/p/w500${movieData.backdrop_path}`;
+
+  return (
+    <li className="item">
+      <div
+        key={movieData.id}
+        className={showExtraInfo ? "itemContainerExtraInfo" : "itemContainer"}
+      >
+        <img
+          src={imgSRC}
+          alt="Poster original de la película"
+          className={!showExtraInfo ? "poster" : "extraInfoPoster"}
+          onClick={extraInfoHandler}
+        />
+        {showExtraInfo && (
+          <div className={showExtraInfo ? "extraInfoShown" : "extraInfoHide"}>
+            <h3 className="extraInfoTitle">{movieData.title}</h3>
+            <p className="extraInfoText">{movieData.overview}</p>
+          </div>
+        )}
+      </div>
+    </li>
+  );
+};
+
+export default MovieItem;
