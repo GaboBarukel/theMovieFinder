b546683b0932039732be60fa331f4233d4a2ba3e
Gabriel Barukel <gabrukel@gmail.com>
2024-05-05 10:56:59 -0300

    starting the searchInput

--- src/Components/Search/SearchForm.jsx
+++ src/Components/Search/SearchForm.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
+
 import "./searchForm.css";
 
 const SearchForm = () => {
+  const [activeInput, setActiveInput] = useState(false);
+
+  function showInputHandler() {
+    setActiveInput(() => !activeInput);
+  }
+
   return (
     <div className="searchBackGradient">
       <div className="searchContainer">
-        <IoSearchSharp className="searchIcon" />
+        <div
+          className={
+            !activeInput
+              ? "inputContainer"
+              : "inputContainer activeInputContainer"
+          }
+        >
+          {activeInput && <input className="searchInput" autoFocus />}
+          <IoSearchSharp className="searchIcon" onClick={showInputHandler} />
+        </div>
       </div>
     </div>
   );
